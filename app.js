d7d42b16b3af3c657a4e69fa865cb97934b044d1
dannybowden <99494482+dannybowden@users.noreply.github.com>
2022-04-16 13:50:28 +0100

    Second Commit
    - Added CSS Styling
    - Created a commentator and football match generator

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 const text = [
-  'Deep into fergie time here', 'It was a great ball in, but there was nobody on the end of it', "he's hit that too well", "I've seen them given", 'A real six pointer', "He didn't quite catch all of it", "He's like an artist, he's got paintbrushes for feet", "But can they do it on a cold Tuesday night in Stoke?", 'A game of two halves', 'Great feet for a big lad', 'A real daisy cutter of a shot',
+  'Deep into fergie time here', 'It was a great ball in, but there was nobody on the end of it', "He's hit that too well", "I've seen them given", 'A real six pointer', "He didn't quite catch all of it", "He's like an artist, he's got paintbrushes for feet", "But can they do it on a cold Tuesday night in Stoke?", 'A game of two halves', 'Great feet for a big lad', 'A real daisy cutter of a shot', "They might have scored too early here",
+  "They have given 110 percent today",
+  "If it was on target, it would have been a goal",
+  "This could be a cricket score",
+  "That was schoolboy defending",
+  "What a brilliant time to score",
+  "You can't win the title in August but you can lose it",
+  "A no-nonsense player",
+  "There's some tired legs out there",
+  "They'll take it one game at a time",
+  "They look like they want it more",
+  "You have to be able to play for the whole 90 minutes",
+  "He's a real throwback",
+  "It looks like they're gonna throw the kitchen sink at it",
+  "He's like a new signing",
+  "This has nil-nil written all over it",
+  "The home side are reknowned for playing football the right way",
+  "Form goes out the window in a game like this",
+  "They're typically slow starters",
+  "There are no easy games",
+  "They look like they have one hand on the trophy here",
+  "The away side are gatecrashing the top 4",
+  "They're running riot here",
+  "He brings something different to the table.",
+  "He has a cultured left foot",
+  "A real wand of a left foot",
+  "They're looking like they are running out of steam",
+  "He's not that sort of player",
+  "The magic of the FA Cup!",
+  "The most important thing is the three points", "He's scored from an impossible angle!"
 ]
 
 const commentator = ['Jermaine Jenas', 'Martin Keown', 'Glenn Hoddle', 'Steve McManaman', 'Danny Murphy', 'Ally McCoist', 'Michael Owen', 'Gary Neville', 'Jamie Carragher', 'Martin Tyler', 'Andy Gray', 'Robbie Savage', 'Andy Townsend', 'Lee Dixon']
@@ -97,117 +126,17 @@ const team1 = ["Accrington Stanley",
 "Wigan Athletic",
 "Wolverhampton Wanderers",
 "Wycombe Wanderers"]
-const team2 = ["Accrington Stanley",
-"AFC Wimbledon",
-"Arsenal",
-"Aston Villa",
-"Barnsley",
-"Barrow AFC",
-"Birmingham City",
-"Blackburn Rovers",
-"Blackpool",
-"Bolton Wanderers",
-"Bournemouth",
-"Bradford City",
-"Brentford",
-"Brighton and Hove Albion",
-"Bristol City",
-"Bristol Rovers",
-"Burnley",
-"Burton Albion",
-"Cambridge United",
-"Cardiff City",
-"Carlisle United",
-"Charlton Athletic",
-"Chelsea",
-"Cheltenham Town",
-"Colchester United",
-"Coventry City",
-"Crawley Town",
-"Crewe Alexandra",
-"Crystal Palace",
-"Derby County",
-"Doncaster Rovers",
-"Everton",
-"Exeter City",
-"Fleetwood Town",
-"Forest Green Rovers",
-"Fulham",
-"Gillingham",
-"Harrogate Town",
-"Hartlepool United",
-"Huddersfield Town",
-"Hull City",
-"Ipswich Town",
-"Leeds United",
-"Leicester City",
-"Leyton Orient",
-"Lincoln City",
-"Liverpool",
-"Luton Town",
-"Macclesfield Town",
-"Manchester City",
-"Manchester United",
-"Mansfield Town",
-"Middlesborough",
-"Millwall",
-"Milton Keynes Dons",
-"Morecambe",
-"Newcastle United",
-"Newport County",
-"Northampton Town",
-"Norwich City",
-"Nottingham Forest",
-"Oldham Athletic",
-"Oxford United",
-"Peterborough United",
-"Plymouth Argyle",
-"Port Vale",
-"Portsmouth",
-"Preston North End",
-"Queens Park Rangers",
-"Reading",
-"Rochdale",
-"Rotherham United",
-"Salford City",
-"Scunthorpe United",
-"Sheffield United",
-"Sheffield Wednesday",
-"Shrewsbury Town",
-"Southampton",
-"Stevenage Borough",
-"Stoke City",
-"Sunderland",
-"Sutton United",
-"Swansea City",
-"Swindon Town",
-"Tottenham Hotspur",
-"Tranmere Rovers",
-"Walsall",
-"Watford",
-"West Bromwich Albion",
-"West Ham United",
-"Wigan Athletic",
-"Wolverhampton Wanderers",
-"Wycombe Wanderers"]
 
 const form = document.querySelector('form');
-const amount = document.getElementById('amount');
 const result = document.getElementById('cliche-text');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  const value = parseInt(amount.value);
-  const random = Math.floor(Math.random() * text.length);
-  
-  if(isNaN(value) || value < 0 || value > 9){
-    result.innerHTML = `<p class='result'>${text[random]}</p>`;
-  } else {
-    let tempText = text.slice(0, value);
-    tempText = tempText.map(function(item){
-      return `<p class='result'>${item}</p>`
-    }).join('');
-    result.innerHTML = tempText;
-  }
-})
+  const cliche = Math.floor(Math.random() * text.length);
+  const person = Math.floor(Math.random() * commentator.length);
+  const first = Math.floor(Math.random() * team1.length);
+  const second = Math.floor(Math.random() * team1.length);
 
+  let quote = `<p class='result'>'${text[cliche]}'</p><p class='match'> - ${commentator[person]} discussing the ${team1[first]} vs. ${team1[second]} game</p>`
+  result.innerHTML= quote;
+})
